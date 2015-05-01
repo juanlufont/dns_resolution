afa6e1c44207a0b54e251254b0fe7dabe492f27c
Juanlu <juanlufont@gmail.com>
2015-05-02 00:54:56 +0200

    better arg parsing, keyboard interruption supported

--- resolve.py
+++ resolve.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
+
 import re
 import sys
+import argparse
 import dns.resolver
 import multiprocessing
 
@@ -44,10 +47,22 @@ if __name__ == '__main__':
     The application receives as input a file with the querys and
     prints on the standard output the DNS resolution for each query
     """
-    nThreads =  64 
 
-    # no fancy argument processing :-/
-    fileName = sys.argv[1]
+    parser = argparse.ArgumentParser(description='Resolves DNS queries from a given file')
+    parser.add_argument('fileName', action='store', type=str, 
+                        help='file containing the DNS queries')
+    parser.add_argument('-t', dest='nThreads', action='store',
+                        type=int, default=4, required=False,
+                        help='number of parallel threads that will process the query file')
+    #parser.add_argument('-o', dest='outputFile', action='store',
+    #                    type=str, required=False,
+    #                    help='output file')
+
+
+    args = parser.parse_args()
+
+    nThreads =  args.nThreads 
+    fileName = args.fileName 
     
     # loading all the query entries from the file
     lines = open(fileName).readlines()
@@ -60,8 +75,13 @@ if __name__ == '__main__':
     pool = multiprocessing.Pool(processes=nThreads)
 
     # assinging blocks of nLines to each worker
-    result_list = pool.map(processLines, 
-                    (lines[line:line+nLines] for line in xrange(0,len(lines),nLines) ) )
+    try:
+        # timeout as trick to get Ctrl+C working
+        result_list = pool.map_async(processLines, 
+                    (lines[line:line+nLines] for line in xrange(0,len(lines),nLines) ) ).get(9999999)
+    except KeyboardInterrupt:
+        pool.terminate()
+        sys.exit(1)
 
     #result = {}
     #for e in result_list:
